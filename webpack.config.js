3dbe7b7d278fe11a095c195306017836797eee86
Vito De Tullio <vito.detullio@finconsgroup.com>
2022-03-30 09:54:40 +0200

    npm serve with autorebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-	entry: './src/index.ts',
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'web'),
-	},
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+            "stream": require.resolve("stream-browserify"),
+            "buffer": require.resolve("buffer")
+        }
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'web'),
+    },
+    plugins: [
+        // Work around for Buffer is undefined:
+        // https://github.com/webpack/changelog-v5/issues/10
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
 };
